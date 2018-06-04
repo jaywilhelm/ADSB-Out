6eaa6068d85a21a714eade8955f7ebd542e47464
JW <jwilhelm@ohio.edu>
2018-06-04 12:45:00 -0400

    Added velocity packet, testing mode

--- ADSB_Encoder.py
+++ ADSB_Encoder.py
@@ -2,28 +2,33 @@
 #
 
 ##########################################################################
-
+# Modification by Jay Wilhelm
+# Added Velocity packet
+# 2018
+#
+#
 # Copyright 2010, 2012 Nick Foster
-# 
+#
 # This file is part of gr-air-modes
-# 
+#
 # gr-air-modes is free software; you can redistribute it and/or modify
 # it under the terms of the GNU General Public License as published by
 # the Free Software Foundation; either version 3, or (at your option)
 # any later version.
-# 
+#
 # gr-air-modes is distributed in the hope that it will be useful,
 # but WITHOUT ANY WARRANTY; without even the implied warranty of
 # MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
 # GNU General Public License for more details.
-# 
+#
 # You should have received a copy of the GNU General Public License
 # along with gr-air-modes; see the file COPYING.  If not, write to
 # the Free Software Foundation, Inc., 51 Franklin Street,
 # Boston, MA 02110-1301, USA.
-# 
+#
 
 import math
+
 
 def encode_alt_modes(alt, bit13):
     mbit = False
@@ -40,56 +45,64 @@ def encode_alt_modes(alt, bit13):
 
     return (encalt & 0x0F) | tmp1 | tmp2 | (mbit << 6) | (qbit << 4)
 
+
 latz = 15
 
+
 def nz(ctype):
-	return 4 * latz - ctype
+    return 4 * latz - ctype
+
 
 def dlat(ctype, surface):
-	if surface == 1:
-		tmp = 90.0
-	else:
-		tmp = 360.0
+    if surface == 1:
+        tmp = 90.0
+    else:
+        tmp = 360.0
 
-	nzcalc = nz(ctype)
-	if nzcalc == 0:
-		return tmp
-	else:
-		return tmp / nzcalc
+    nzcalc = nz(ctype)
+    if nzcalc == 0:
+        return tmp
+    else:
+        return tmp / nzcalc
+
 
 def nl(declat_in):
-	if abs(declat_in) >= 87.0:
-		return 1.0
-	return math.floor( (2.0*math.pi) * math.acos(1.0- (1.0-math.cos(math.pi/(2.0*latz))) / math.cos( (math.pi/180.0)*abs(declat_in) )**2 )**-1)
+    if abs(declat_in) >= 87.0:
+        return 1.0
+    return math.floor((2.0 * math.pi) * math.acos(
+        1.0 - (1.0 - math.cos(math.pi / (2.0 * latz))) / math.cos((math.pi / 180.0) * abs(declat_in)) ** 2) ** -1)
+
 
 def dlon(declat_in, ctype, surface):
-	if surface:
-		tmp = 90.0
-	else:
-		tmp = 360.0
-	nlcalc = max(nl(declat_in)-ctype, 1)
-	return tmp / nlcalc
+    if surface:
+        tmp = 90.0
+    else:
+        tmp = 360.0
+    nlcalc = max(nl(declat_in) - ctype, 1)
+    return tmp / nlcalc
 
-#encode CPR position
+
+# encode CPR position
 def cpr_encode(lat, lon, ctype, surface):
     if surface is True:
-        scalar = 2.**19
+        scalar = 2. ** 19
     else:
-        scalar = 2.**17
+        scalar = 2. ** 17
 
-    #encode using 360 constant for segment size.
+    # encode using 360 constant for segment size.
     dlati = dlat(ctype, False)
-    yz = math.floor(scalar * ((lat % dlati)/dlati) + 0.5)
+    yz = math.floor(scalar * ((lat % dlati) / dlati) + 0.5)
     rlat = dlati * ((yz / scalar) + math.floor(lat / dlati))
-    
-    #encode using 360 constant for segment size.
+
+    # encode using 360 constant for segment size.
     dloni = dlon(lat, ctype, False)
-    xz = math.floor(scalar * ((lon % dloni)/dloni) + 0.5)
-    
-    yz = int(yz) & (2**17-1)
-    xz = int(xz) & (2**17-1)
-    
-    return (yz, xz) #lat, lon
+    xz = math.floor(scalar * ((lon % dloni) / dloni) + 0.5)
+
+    yz = int(yz) & (2 ** 17 - 1)
+    xz = int(xz) & (2 ** 17 - 1)
+
+    return (yz, xz)  # lat, lon
+
 
 ###############################################################
 
@@ -111,6 +124,7 @@ def cpr_encode(lat, lon, ctype, surface):
 # the polynominal generattor code for CRC
 GENERATOR = "1111111111111010000001001"
 
+
 def hex2bin(hexstr):
     """Convert a hexdecimal string to binary string, with zero fillings. """
     scale = 16
@@ -118,9 +132,11 @@ def hex2bin(hexstr):
     binstr = bin(int(hexstr, scale))[2:].zfill(int(num_of_bits))
     return binstr
 
+
 def bin2int(binstr):
     """Convert a binary string to integer. """
     return int(binstr, 2)
+
 
 def crc(msg, encode=False):
     """Mode-S Cyclic Redundancy Check
@@ -138,17 +154,18 @@ def crc(msg, encode=False):
         msgbin[-24:] = ['0'] * 24
 
     # loop all bits, except last 24 piraty bits
-    for i in range(len(msgbin)-24):
+    for i in range(len(msgbin) - 24):
         # if 1, perform modulo 2 multiplication,
         if msgbin[i] == '1':
             for j in range(len(GENERATOR)):
                 # modulo 2 multiplication = XOR
-                msgbin[i+j] = str((int(msgbin[i+j]) ^ int(GENERATOR[j])))
+                msgbin[i + j] = str((int(msgbin[i + j]) ^ int(GENERATOR[j])))
 
     # last 24 bits
     reminder = ''.join(msgbin[-24:])
     return reminder
-    
+
+
 ###############################################################
 
 '''
@@ -166,40 +183,44 @@ def crc(msg, encode=False):
    See the License for the specific language governing permissions and
    limitations under the License.
 '''
+
+
 def bin2dec(buf):
-  if 0 == len(buf): # Crap input
-    return -1
-  return int(buf, 2)
+    if 0 == len(buf):  # Crap input
+        return -1
+    return int(buf, 2)
+
 
 # Ported from: http://www.radarspotters.eu/forum/index.php?topic=5617.msg41293#msg41293
 def get_parity(msg, extended):
-  msg_length = len(msg)
-  payload = msg[:msg_length - 24]
-  parity = msg[msg_length - 24:]
+    msg_length = len(msg)
+    payload = msg[:msg_length - 24]
+    parity = msg[msg_length - 24:]
 
-  data = bin2dec(payload[0:32])
-  if extended:
-    data1 = bin2dec(payload[32:64])
-    data2 = bin2dec(payload[64:]) << 8
-
-  hex_id = bin2dec(parity) << 8
-
-  for i in range(0, len(payload)):
-    if ((data & 0x80000000) != 0):
-      data ^= 0xFFFA0480
-    data <<= 1
-
+    data = bin2dec(payload[0:32])
     if extended:
-      if ((data1 & 0x80000000) != 0):
-        data |= 1
-      data1 <<= 1
+        data1 = bin2dec(payload[32:64])
+        data2 = bin2dec(payload[64:]) << 8
 
-      if ((data2 & 0x80000000) != 0):
-        data1 = data1 | 1
-      data2 <<= 1
+    hex_id = bin2dec(parity) << 8
 
-  return data
-  #return (data ^ hex_id) >> 8
+    for i in range(0, len(payload)):
+        if ((data & 0x80000000) != 0):
+            data ^= 0xFFFA0480
+        data <<= 1
+
+        if extended:
+            if ((data1 & 0x80000000) != 0):
+                data |= 1
+            data1 <<= 1
+
+            if ((data2 & 0x80000000) != 0):
+                data1 = data1 | 1
+            data2 <<= 1
+
+    return data
+    # return (data ^ hex_id) >> 8
+
 
 ###############################################################
 
@@ -215,12 +236,14 @@ Manchester encoding and decoding is also included, and by default will use
 least bit ordering for the byte that is to be included in the array.
 """
 
+
 def extract_bit(byte, pos):
     """
     Extract a bit from a given byte using MS ordering.
     ie. B7 B6 B5 B4 B3 B2 B1 B0
     """
     return (byte >> pos) & 0x01
+
 
 def manchester_encode(byte):
     """
@@ -246,6 +269,7 @@ def manchester_encode(byte):
 
     return manchester_encoded
 
+
 ###############################################################
 
 # Copyright (C) 2017-2018 Linar Yusupov
@@ -264,101 +288,162 @@ def manchester_encode(byte):
 # along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
 import numpy
-#from scipy.signal import hilbert
+
+def vel_heading_encode():
+    #(ca,icao,ew_dir,ew_vel,ns_dir,ns_vel)
+    df = 17
+    ca = 5
+
+    #1-5    downlink format
+    #6-8    CA capability
+    #9-32   ICAO
+    #33-88  DATA -> 33-87 w/ 33-37 TC
+    #89-112 Parity
+
+    tc = 19     #33-37  1-5 type code
+    st = 0x01   #38-40  6-8 subtype, 3 air, 1 ground speed
+    ic = 0 #      #41     9 intent change flag
+    resv_a = 0#1  #42     10
+    NAC = 2#0     #43-45  11-13 velocity uncertainty
+    S_EW = 1#1    #46     14
+    V_EW = 97#9    #47-56  15-24
+    S_NS = 0#1    #57     25 north-south sign
+    V_NS = 379#0xA0 #58-67  26-35 160 north-south vel
+    VrSrc = 1#0   #68     36 vertical rate source
+    S_Vr = 1#1    #69     37 vertical rate sign
+    Vr = 41#0x0E   #70-78  38-46 14 vertical rate
+    RESV_B = 0  #79-80  47-48
+    S_Dif = 0   #81     49 diff from baro alt, sign
+    Dif = 0x1c#0x17  #82-88  50-66 23 diff from baro alt
+
+    ca = 5
+    icao = 0xabcdef#0xa06703 #0x485020 #
+
+    dfvel = []
+    dfvel.append((df << 3) | ca)
+    dfvel.append((icao >> 16) & 0xff)
+    dfvel.append((icao >> 8) & 0xff)
+    dfvel.append((icao) & 0xff)
+    # data
+    dfvel.append((tc << 3) | st)
+    dfvel.append((ic << 7) | (resv_a << 6) | (NAC << 3) | (S_EW << 2) | ((V_EW >> 8) & 0x03))
+    dfvel.append(0xFF & V_EW)
+    dfvel.append((S_NS << 7) | ((V_NS >> 3))) #& 0x7F))
+    dfvel.append(((V_NS << 5) & 0xE0) | (VrSrc << 4) | (S_Vr << 3) | ((Vr >> 6) & 0x03))
+    dfvel.append(((Vr  << 2) & 0xFC) | (RESV_B))
+    dfvel.append((S_Dif << 7) | (Dif))
+
+    dfvel_str = "{0:02x} {1:02x} {2:02x} {3:02x} {4:02x} {5:02x} {6:02x} {7:02x} {8:02x} {9:02x} {10:02x}".format(
+        *dfvel[0:11])
+    print(dfvel_str)
+    dfvel_str2 = "{0:02x}{1:02x}{2:02x}{3:02x}{4:02x}{5:02x}{6:02x}{7:02x}{8:02x}{9:02x}{10:02x}".format(
+        *dfvel[0:11])
+    crc_str = "%X" % bin2int(crc(dfvel_str2+"000000", encode=True))
+    print(crc_str)
+    #print(dfvel_str), " %X" % +"000000", encode=True))
+    #, "%X" % get_parity(hex2bin(dfvel_str+"000000"), extended=True))
+    dfvel_crc = bin2int(crc(dfvel_str2 + "000000", encode=True))
+    dfvel.append((dfvel_crc >> 16) & 0xff)
+    dfvel.append((dfvel_crc >> 8) & 0xff)
+    dfvel.append((dfvel_crc) & 0xff)
+    return dfvel
+# from scipy.signal import hilbert
 
 def df17_pos_rep_encode(ca, icao, tc, ss, nicsb, alt, time, lat, lon, surface):
-
     format = 17
 
-    enc_alt =	encode_alt_modes(alt, surface)
-    #print "Alt(%r): %X " % (surface, enc_alt)
+    enc_alt = encode_alt_modes(alt, surface)
+    # print "Alt(%r): %X " % (surface, enc_alt)
 
-    #encode that position
+    # encode that position
     (evenenclat, evenenclon) = cpr_encode(lat, lon, False, surface)
-    (oddenclat, oddenclon)   = cpr_encode(lat, lon, True, surface)
+    (oddenclat, oddenclon) = cpr_encode(lat, lon, True, surface)
 
-    #print "Even Lat/Lon: %X/%X " % (evenenclat, evenenclon)
-    #print "Odd  Lat/Lon: %X/%X " % (oddenclat, oddenclon)
+    # print "Even Lat/Lon: %X/%X " % (evenenclat, evenenclon)
+    # print "Odd  Lat/Lon: %X/%X " % (oddenclat, oddenclon)
 
     ff = 0
     df17_even_bytes = []
-    df17_even_bytes.append((format<<3) | ca)
-    df17_even_bytes.append((icao>>16) & 0xff)
-    df17_even_bytes.append((icao>> 8) & 0xff)
-    df17_even_bytes.append((icao    ) & 0xff)
+    df17_even_bytes.append((format << 3) | ca)
+    df17_even_bytes.append((icao >> 16) & 0xff)
+    df17_even_bytes.append((icao >> 8) & 0xff)
+    df17_even_bytes.append((icao) & 0xff)
     # data
-    df17_even_bytes.append((tc<<3) | (ss<<1) | nicsb)
-    df17_even_bytes.append((enc_alt>>4) & 0xff)
-    df17_even_bytes.append((enc_alt & 0xf) << 4 | (time<<3) | (ff<<2) | (evenenclat>>15))
-    df17_even_bytes.append((evenenclat>>7) & 0xff)    
-    df17_even_bytes.append(((evenenclat & 0x7f) << 1) | (evenenclon>>16))  
-    df17_even_bytes.append((evenenclon>>8) & 0xff)   
-    df17_even_bytes.append((evenenclon   ) & 0xff)
+    df17_even_bytes.append((tc << 3) | (ss << 1) | nicsb)
+    df17_even_bytes.append((enc_alt >> 4) & 0xff)
+    df17_even_bytes.append((enc_alt & 0xf) << 4 | (time << 3) | (ff << 2) | (evenenclat >> 15))
+    df17_even_bytes.append((evenenclat >> 7) & 0xff)
+    df17_even_bytes.append(((evenenclat & 0x7f) << 1) | (evenenclon >> 16))
+    df17_even_bytes.append((evenenclon >> 8) & 0xff)
+    df17_even_bytes.append((evenenclon) & 0xff)
 
-    df17_str = "{0:02x}{1:02x}{2:02x}{3:02x}{4:02x}{5:02x}{6:02x}{7:02x}{8:02x}{9:02x}{10:02x}".format(*df17_even_bytes[0:11])
-    #print df17_str , "%X" % bin2int(crc(df17_str+"000000", encode=True)) , "%X" % get_parity(hex2bin(df17_str+"000000"), extended=True)
-    df17_crc = bin2int(crc(df17_str+"000000", encode=True))
+    df17_str = "{0:02x}{1:02x}{2:02x}{3:02x}{4:02x}{5:02x}{6:02x}{7:02x}{8:02x}{9:02x}{10:02x}".format(
+        *df17_even_bytes[0:11])
+    # print df17_str , "%X" % bin2int(crc(df17_str+"000000", encode=True)) , "%X" % get_parity(hex2bin(df17_str+"000000"), extended=True)
+    df17_crc = bin2int(crc(df17_str + "000000", encode=True))
 
-    df17_even_bytes.append((df17_crc>>16) & 0xff)
-    df17_even_bytes.append((df17_crc>> 8) & 0xff)
-    df17_even_bytes.append((df17_crc    ) & 0xff)
- 
+    df17_even_bytes.append((df17_crc >> 16) & 0xff)
+    df17_even_bytes.append((df17_crc >> 8) & 0xff)
+    df17_even_bytes.append((df17_crc) & 0xff)
+
     ff = 1
     df17_odd_bytes = []
-    df17_odd_bytes.append((format<<3) | ca)
-    df17_odd_bytes.append((icao>>16) & 0xff)
-    df17_odd_bytes.append((icao>> 8) & 0xff)
-    df17_odd_bytes.append((icao    ) & 0xff)
+    df17_odd_bytes.append((format << 3) | ca)
+    df17_odd_bytes.append((icao >> 16) & 0xff)
+    df17_odd_bytes.append((icao >> 8) & 0xff)
+    df17_odd_bytes.append((icao) & 0xff)
     # data
-    df17_odd_bytes.append((tc<<3) | (ss<<1) | nicsb)
-    df17_odd_bytes.append((enc_alt>>4) & 0xff)
-    df17_odd_bytes.append((enc_alt & 0xf) << 4 | (time<<3) | (ff<<2) | (oddenclat>>15))
-    df17_odd_bytes.append((oddenclat>>7) & 0xff)    
-    df17_odd_bytes.append(((oddenclat & 0x7f) << 1) | (oddenclon>>16))  
-    df17_odd_bytes.append((oddenclon>>8) & 0xff)   
-    df17_odd_bytes.append((oddenclon   ) & 0xff)
+    df17_odd_bytes.append((tc << 3) | (ss << 1) | nicsb)
+    df17_odd_bytes.append((enc_alt >> 4) & 0xff)
+    df17_odd_bytes.append((enc_alt & 0xf) << 4 | (time << 3) | (ff << 2) | (oddenclat >> 15))
+    df17_odd_bytes.append((oddenclat >> 7) & 0xff)
+    df17_odd_bytes.append(((oddenclat & 0x7f) << 1) | (oddenclon >> 16))
+    df17_odd_bytes.append((oddenclon >> 8) & 0xff)
+    df17_odd_bytes.append((oddenclon) & 0xff)
 
-    df17_str = "{0:02x}{1:02x}{2:02x}{3:02x}{4:02x}{5:02x}{6:02x}{7:02x}{8:02x}{9:02x}{10:02x}".format(*df17_odd_bytes[0:11])
-    df17_crc = bin2int(crc(df17_str+"000000", encode=True))
+    df17_str = "{0:02x}{1:02x}{2:02x}{3:02x}{4:02x}{5:02x}{6:02x}{7:02x}{8:02x}{9:02x}{10:02x}".format(
+        *df17_odd_bytes[0:11])
+    df17_crc = bin2int(crc(df17_str + "000000", encode=True))
 
-    df17_odd_bytes.append((df17_crc>>16) & 0xff)
-    df17_odd_bytes.append((df17_crc>> 8) & 0xff)
-    df17_odd_bytes.append((df17_crc    ) & 0xff)    
-    
+    df17_odd_bytes.append((df17_crc >> 16) & 0xff)
+    df17_odd_bytes.append((df17_crc >> 8) & 0xff)
+    df17_odd_bytes.append((df17_crc) & 0xff)
+
     return (df17_even_bytes, df17_odd_bytes)
 
+
 def frame_1090es_ppm_modulate(even, odd):
-    ppm = [ ]
+    ppm = []
 
-    for i in range(48):    # pause
-        ppm.append( 0 )
+    for i in range(48):  # pause
+        ppm.append(0)
 
-    ppm.append( 0xA1 )   # preamble
-    ppm.append( 0x40 )
-    
+    ppm.append(0xA1)  # preamble
+    ppm.append(0x40)
+
     for i in range(len(even)):
         word16 = numpy.packbits(manchester_encode(~even[i]))
         ppm.append(word16[0])
         ppm.append(word16[1])
 
+    for i in range(100):  # pause
+        ppm.append(0)
 
-    for i in range(100):    # pause
-        ppm.append( 0 )
-
-    ppm.append( 0xA1 )   # preamble
-    ppm.append( 0x40 )
+    ppm.append(0xA1)  # preamble
+    ppm.append(0x40)
 
     for i in range(len(odd)):
         word16 = numpy.packbits(manchester_encode(~odd[i]))
         ppm.append(word16[0])
         ppm.append(word16[1])
 
-    for i in range(48):    # pause
-        ppm.append( 0 )
+    for i in range(48):  # pause
+        ppm.append(0)
 
-    #print '[{}]'.format(', '.join(hex(x) for x in ppm))
-    
+    # print '[{}]'.format(', '.join(hex(x) for x in ppm))
+
     return bytearray(ppm)
+
 
 def hackrf_raw_IQ_format(ppm):
     """
@@ -390,19 +475,33 @@ def hackrf_raw_IQ_format(ppm):
         signal.append(Q)
 
     return bytearray(signal)
-    
+
+
 if __name__ == "__main__":
 
     from sys import argv, exit
-    
+
+    df17_array = vel_heading_encode()
+    frame_array = frame_1090es_ppm_modulate(df17_array,df17_array)
+    samples_array = hackrf_raw_IQ_format(frame_array)
+
+    SamplesFile = open("SamplesVel.iq8s", "wb")
+    SamplesFile.write(samples_array)
+    exit(1)
+
     argc = len(argv)
     if argc != 5:
-      print
-      print 'Usage: '+ argv[0] +'  <ICAO> <Latitude> <Longtitude> <Altitude>'
-      print
-      print '    Example: '+ argv[0] +'  0xABCDEF 12.34 56.78 9999.0'
-      print
-      exit(2)
+        print
+        print
+        'Usage: ' + argv[0] + '  <ICAO> <Latitude> <Longtitude> <Altitude>'
+        print
+        print
+        '    Example: ' + argv[0] + '  0xABCDEF 12.34 56.78 9999.0'
+        print
+        exit(2)
+
+
+
 
     icao = int(argv[1], 16)
     lat = float(argv[2])
@@ -412,19 +511,19 @@ if __name__ == "__main__":
     ca = 5
     tc = 11
     ss = 0
-    nicsb = 0    
-    time = 0       
+    nicsb = 0
+    time = 0
     surface = False
 
     (df17_even, df17_odd) = df17_pos_rep_encode(ca, icao, tc, ss, nicsb, alt, time, lat, lon, surface)
 
-    #print ''.join(format(x, '02x') for x in df17_even)
-    #print ''.join(format(x, '02x') for x in df17_odd)
+    # print ''.join(format(x, '02x') for x in df17_even)
+    # print ''.join(format(x, '02x') for x in df17_odd)
 
     df17_array = frame_1090es_ppm_modulate(df17_even, df17_odd)
 
-    #OutFile = open("filename.bin", "wb")
-    #OutFile.write(df17_array)
+    # OutFile = open("filename.bin", "wb")
+    # OutFile.write(df17_array)
 
     samples_array = hackrf_raw_IQ_format(df17_array)
 
